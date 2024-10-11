15f5c8d7b427bdd9209d4c16a29071bed61c0aa0
jorge <jorge@INSPIRON153511>
2024-10-11 13:58:10 -0600

    Se inyecto el Repositorio En el repositorio se implementaron el Get, el Post sin retorno y el Post con retorno

--- WebConta.Frontend/Program.cs
+++ WebConta.Frontend/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WebConta.Frontend;
+using WebConta.Frontend.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Esta es la url que provee los servicios del backend
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7166//") });
+
+//Inyectamos el Repositorio porque el Frontend solo ebtiene datos de repositorio
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
---
+++ WebConta.Frontend/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace WebConta.Frontend.Repositories
+{
+    public interface IRepository
+    {
+        //Los metodos son genericos <T> para cualquier entidad
+        //La url es la del controlador
+        //El va a devolver un HttpResponseWrapper <T> para cualquier entidad
+        Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+
+        //Se sobreescribe el PostAsync parque hay Post que no regresan nada y otros que si
+        Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+        Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+    }
+}
