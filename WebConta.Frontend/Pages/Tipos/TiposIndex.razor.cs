283dd029770c7825085f2207e62325dd40c70d3d
jorge <jorge@INSPIRON153511>
2024-10-11 21:15:10 -0600

    Separamos el Code Behind de Tipos Se implemento el GenericList

---
+++ WebConta.Frontend/Pages/Tipos/TiposIndex.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using WebConta.Frontend.Repositories;
+using WebConta.Shared.Entities;
+
+namespace WebConta.Frontend.Pages.Tipos
+{
+    public partial class TiposIndex
+    {
+        /*Inyectamos el Repositorio pero el objeto tiene que ir en con la primera en Mayuscula tipo propiedad con {get; y set;} lo injectamos en la clase _Imports.razor */
+        [Inject] private IRepository Repository { get; set; } = null!;
+
+        //Creamos una lista tipo countries con ? porque puede ser null
+        public List<Tipo>? LTipos { get; set; }
+
+        //Sobre cargamos el metodo qe se ejecuta cuando carga la pagina
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            //Obtenemos una lista de paises utilizando el componente repository generico que creamos
+            //Utilizamos el GetAsync al que se le manda la url "api/tipos" y el devuelve el responseHttp con todo el contenido de respuesta, en este caso una lista
+
+            var responsehttp = await Repository.GetAsync<List<Tipo>>("api/tipos");
+            LTipos = responsehttp.Response;
+        }
+    }
+}
---
+++ WebConta.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebConta.Frontend.Shared
+{
+    //Lista generica con Titem de parametro
+    public partial class GenericList<Titem>
+    {
+        //Este parametro sirve para deplegar cuando carga el componente
+        [Parameter] public RenderFragment? Loading { get; set; }
+
+        //Esto es lo que va a desplegar cuando la lista este vacia
+        [Parameter] public RenderFragment? NoRecords { get; set; }
+
+        //Este sirve para desplegar en el body y es obligatorio
+        [EditorRequired, Parameter] public RenderFragment? Body { get; set; } = null!;
+
+        //Cual es la lista que se desea desplegar
+        [EditorRequired, Parameter] public List<Titem>? MyList { get; set; } = null!;
+    }
+}
